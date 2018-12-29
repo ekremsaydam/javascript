92654a83d154fae4e409196fdc466b907e32552a
ekremsaydam <30255407+ekremsaydam@users.noreply.github.com>
2018-12-29 23:07:24 +0300

    document object

--- 17_domManipulasyonu/app.js
+++ 17_domManipulasyonu/app.js
@@ -1,20 +1,26 @@
 let value;
-
+// Scripts
 value = document.scripts;
 value = document.scripts.length;
 value = document.scripts[0];
-
+// Linkler
 value = document.links;
 value = document.links.length;
 value = document.links[4];
+value = document.links[document.links.length - 1];
 value = document.links[document.links.length - 1].getAttribute("class");
 value = document.links[document.links.length - 1].getAttribute("href");
+value = document.links[document.links.length - 1].className;
 value = document.links[document.links.length - 1].classList;
-
+// Formlar
 value = document.forms;
 value = document.forms.length;
-value = document.forms[0];
-value = document.forms["form"]
-
+// value = document.forms[0];
+value = document.forms['form'];
+value = document.forms[0].id;
+value = document.forms[0].getAttribute('id');
+value = document.forms[0].name;
+value = document.forms[0].getAttribute('name');
+value = document.forms[0].method;
 
 console.log(value);
