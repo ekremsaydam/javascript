c40132242adcec1881d41dff82155602ddeec29d
ekremsaydam <30255407+ekremsaydam@users.noreply.github.com>
2018-12-30 18:05:47 +0300

    Arrow Function

--- 33_es6_arrowFunction/app.js
+++ 33_es6_arrowFunction/app.js
@@ -1,1 +1,38 @@
-console.log("Test");
+// const merhaba = function () {
+//     console.log("Merhaba");
+// }
+
+// merhaba();
+
+// Arrow function
+// const merhaba = () => {
+//     console.log("Merhaba");
+// }
+
+// merhaba();
+
+// const merhaba = (firstName, lastName) => {
+//     console.log("Merhaba", firstName, lastName);
+// }
+
+// merhaba("Zeki", "Koşar");
+
+//Tek parametreli ise parantezleri kaldırılabilir.
+// const merhaba = firstName => console.log("Merhaba", firstName);
+// const merhaba = (firstName, lastName) => console.log("Merhaba", firstName, lastName);
+// merhaba("Zeki", "Koşar");
+
+
+
+// const cube = function (x) {
+//     return x * x * x;
+// }
+
+// console.log(cube(4));
+
+// const cube = x => {
+//     return x * x * x;
+// }
+
+const cube = x => x * x * x;
+console.log(cube(4));
