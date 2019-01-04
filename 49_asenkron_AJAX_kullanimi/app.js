904c43489ad9e80491fbd89a44441480686a3d1c
ekremsaydam <30255407+ekremsaydam@users.noreply.github.com>
2019-01-04 22:30:50 +0300

    XMLHttpRequest, onreadystatechange, onprogress, onload, responseText, readyState, status

--- 49_asenkron_AJAX_kullanimi/app.js
+++ 49_asenkron_AJAX_kullanimi/app.js
@@ -3,3 +3,48 @@
 // 403 : Forbidden
 // 404 : Not Found
 // 505 : Internal Server console.error();
+
+// readyState	Holds the status of the XMLHttpRequest.
+// 0: request not initialized 
+// 1: server connection established
+// 2: request received 
+// 3: processing request 
+// 4: request finished and response is ready
+
+document.getElementById("btn").addEventListener("click", function() {
+    // XMLHttpRequest
+
+    const xhr = new XMLHttpRequest();
+    // console.log(xhr);
+    // xhr.onreadystatechange = function () {
+    //     // console.log(this.readyState);
+    //     // console.log(this.status);
+    //     if (this.status == 200 && this.readyState == 4) {
+    //         // Response Hazır
+    //         console.log(this.responseText);
+    //     }
+    // };
+    xhr.onprogress = function () {
+        // readystate 3 olduğunda çalışır.
+        // console.log("Progress işleniyor.");
+    };
+
+    xhr.onload = function () {
+        // readystate 4 olduğunda çalışır.
+        // console.log(this.readyState);
+        // console.log(this.status);
+        if (this.status == 200) {
+            // herhangi bir sorun olmadan veri alınmıştır.
+            // console.log(this.responseText);
+            document.getElementById("ajax").textContent = this.responseText;
+        }
+    };
+
+    xhr.open("GET", "example.txt", true);
+
+    xhr.send();
+
+
+
+
+});
