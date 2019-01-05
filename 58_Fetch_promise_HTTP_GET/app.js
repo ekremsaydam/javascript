9deb0284d7ab47a43e36ac055ab14c3a829bfd49
ekremsaydam <30255407+ekremsaydam@users.noreply.github.com>
2019-01-06 00:13:16 +0300

    fetch POST

--- 58_Fetch_promise_HTTP_GET/app.js
+++ 58_Fetch_promise_HTTP_GET/app.js
@@ -2,19 +2,40 @@ class Request {
     get(url) { // GET Request
         return new Promise((resolve, reject) => {
             fetch(url)
-            .then(response => response.json())
-            .then(data => resolve(data))
-            .catch(err => reject(err));
-        })
+                .then(response => response.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err));
+        });
+
+    }
+
+    post(url, data) {
+        
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            })
+                .then(response => response.json())
+                .then(json => resolve(json))
+                .catch(err => reject(err));
+        });
         
     }
 }
 
 const r = new Request();
-let albums;
+// let albums;
 
-r.get("https://jsonplaceholder.typicode.com/albums")
-    .then(albums => console.log(albums))
-    .catch(err => console.log(err));
+// r.get("https://jsonplaceholder.typicode.com/albums")
+//     .then(albums => console.log(albums))
+//     .catch(err => console.log(err));
 
 // console.log(albums);
+
+r.post("https://jsonplaceholder.typicode.com/albums", { userId: 1, title: "Tarkan - 10" })
+    .then(newAlbum => console.log(newAlbum))
+    .catch(err => console.log(err));
