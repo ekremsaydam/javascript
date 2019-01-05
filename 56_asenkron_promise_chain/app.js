86e5fab948aa9c7da1e14637e968ce4bce25a204
ekremsaydam <30255407+ekremsaydam@users.noreply.github.com>
2019-01-05 15:00:52 +0300

    Promise Chain then().then().catch()

--- 56_asenkron_promise_chain/app.js
+++ 56_asenkron_promise_chain/app.js
@@ -1,24 +1,46 @@
-function getData(data) { //promise Objesi döndüren fonksion
-    return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-            // resolve("Olumlu sonuç");
-            // reject("Olumsuz sonuç");
-            if (typeof data == "string") {
-                resolve("Gelen Değer : " + data);
+// function getData(data) { //promise Objesi döndüren fonksion
+//     return new Promise(function(resolve, reject) {
+//         setTimeout(function() {
+//             // resolve("Olumlu sonuç");
+//             // reject("Olumsuz sonuç");
+//             if (typeof data == "string") {
+//                 resolve("Gelen Değer : " + data);
+//             } else {
+//                 reject(new Error("Lütfen string bir değer giriniz."));
+//             }
+//         }, 2000);
+//     });
+// }
+
+// // console.log(getData("Merhaba"));
+// // getData(1).then(function(response) {
+// //     console.log(response);
+// // }).catch(function(err) {
+// //     console.error(err);
+// // });
+
+// getData(1)
+//     .then((res) => console.log(res))
+//     .catch((err) => console.error(err));
+
+function addTwo(number) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof number == "number") {
+                resolve(number + 2);
             } else {
-                reject(new Error("Lütfen string bir değer giriniz."));
+                reject(new Error("Lütfen bir sayı giriniz."));
             }
-        }, 2000);
+        }, 3000);
     });
 }
 
-// console.log(getData("Merhaba"));
-// getData(1).then(function(response) {
-//     console.log(response);
-// }).catch(function(err) {
-//     console.error(err);
-// });
+addTwo("10")
+    .then(response => {
+        console.log(response);
+        return response + 2;
+    }).then((response2) => console.log(response2))
+    .catch((err) => console.error(err)); // Yanlızca bir tane catch birden çok then kullanılabilir.
+// Birden çok then - Promise Chain
 
-getData("Merhaba")
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
+addTwo(10);
