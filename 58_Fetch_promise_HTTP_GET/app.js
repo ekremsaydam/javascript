58b865a345f0311e1eb6985679cfed1389435325
ekremsaydam <30255407+ekremsaydam@users.noreply.github.com>
2019-01-06 00:23:27 +0300

    fetch PUT, DELETE

--- 58_Fetch_promise_HTTP_GET/app.js
+++ 58_Fetch_promise_HTTP_GET/app.js
@@ -10,17 +10,44 @@ class Request {
     }
 
     post(url, data) {
-        
+
         return new Promise((resolve, reject) => {
             fetch(url, {
-                method: 'POST',
-                body: JSON.stringify(data),
-                headers: {
-                    "Content-type": "application/json; charset=UTF-8"
-                }
-            })
+                    method: 'POST',
+                    body: JSON.stringify(data),
+                    headers: {
+                        "Content-type": "application/json; charset=UTF-8"
+                    }
+                })
                 .then(response => response.json())
                 .then(json => resolve(json))
+                .catch(err => reject(err));
+        });
+
+    }
+
+    put(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                    method: 'PUT',
+                    body: JSON.stringify(data),
+                    headers: {
+                        "Content-type": "application/json; charset=UTF-8"
+                    }
+                })
+                .then(response => response.json())
+                .then(json => resolve(json))
+                .catch(err => reject(err));
+        });
+
+    }
+
+    delete(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then((data) => resolve("Veri Silme işlemi başarılı"))
                 .catch(err => reject(err));
         });
         
@@ -36,6 +63,17 @@ const r = new Request();
 
 // console.log(albums);
 
-r.post("https://jsonplaceholder.typicode.com/albums", { userId: 1, title: "Tarkan - 10" })
-    .then(newAlbum => console.log(newAlbum))
+// r.post("https://jsonplaceholder.typicode.com/albums", { userId: 1, title: "Tarkan - 10" })
+//     .then(newAlbum => console.log(newAlbum))
+//     .catch(err => console.log(err));
+
+// r.put("https://jsonplaceholder.typicode.com/albums/1", {
+//         userId: 10,
+//         title: "Tarkan - Karma"
+//     })
+//     .then((data) => console.log(data))
+//     .catch(err => console.log(err));
+
+r.delete("https://jsonplaceholder.typicode.com/albums/1")
+    .then(data => console.log(data))
     .catch(err => console.log(err));
