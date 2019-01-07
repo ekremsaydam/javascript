ad68c2b6b440d7cc3a125090a8acfbf2b3105be6
ekremsaydam <30255407+ekremsaydam@users.noreply.github.com>
2019-01-07 22:35:44 +0300

    global packet use

---
+++ 65_global_yerel_paket/packet1/index.js
@@ -0,0 +1,4 @@
+console.log('Packet1');
+const is = require('simple-is');
+
+console.log(is.number(24));
---
+++ 65_global_yerel_paket/packet2/index.js
@@ -0,0 +1,4 @@
+console.log('Packet2');
+const is = require('simple-is');
+
+console.log(is.number(24));
